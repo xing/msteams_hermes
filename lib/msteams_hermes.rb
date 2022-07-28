9f7ee42027268aaa5afeaf8d6dd6d197e3ffa989
Rafael Rocha <rafael.rocha@xing.com>
2022-07-28 16:21:57 +0200

    init gem

---
+++ lib/msteams_hermes.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "msteams_hermes/version"
+
+module MsTeamsHermes
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/msteams_hermes_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe MsTeamsHermes do
+  it "has a version number" do
+    expect(MsTeamsHermes::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
