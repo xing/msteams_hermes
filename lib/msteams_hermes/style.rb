60f6f8cfa1cc0c38fa1885565ff37f3e39fbbf11
Rafael Rocha <rafael.rocha@xing.com>
2022-08-03 17:11:30 +0200

    Create container_style.rb

--- lib/msteams_hermes/style.rb
+++ lib/msteams_hermes/style.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "msteams_hermes/style/colors"
+require "msteams_hermes/style/container_style"
 require "msteams_hermes/style/font_size"
 require "msteams_hermes/style/font_type"
 require "msteams_hermes/style/font_weight"
@@ -8,6 +9,7 @@ require "msteams_hermes/style/font_weight"
 module MsTeamsHermes
   module Style
     include MsTeamsHermes::Style::Colors
+    include MsTeamsHermes::Style::ContainerStyle
     include MsTeamsHermes::Style::FontSize
     include MsTeamsHermes::Style::FontType
     include MsTeamsHermes::Style::FontWeight
---
+++ lib/msteams_hermes/style/container_style.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module MsTeamsHermes
+  module Style
+    module ContainerStyle
+      DEFAULT = "default"
+      ACCENT = "accent"
+      GOOD = "good"
+      WARNING = "warning"
+      ATTENTION = "attention"
+      EMPHASIS = "emphasis"
+    end
+  end
+end
