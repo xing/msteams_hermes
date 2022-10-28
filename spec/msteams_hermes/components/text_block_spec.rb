56cc8f744effe37c645fda506a2e41f96c5a4798
Michael Jäger <2405937+huNt-FMJ@users.noreply.github.com>
2022-10-28 17:02:14 +0200

    [text_block_spec.rb]: Fix rspec test

--- spec/msteams_hermes/components/text_block_spec.rb
+++ spec/msteams_hermes/components/text_block_spec.rb
@@ -19,7 +19,8 @@ RSpec.describe MsTeamsHermes::Components::TextBlock do
                                                size: size,
                                                font_type: font_type,
                                                weight: weight,
-                                               wrap: wrap)
+                                               wrap: wrap,
+                                               is_subtle: is_subtle)
     end
 
     let(:text) { "any text" }
@@ -28,6 +29,7 @@ RSpec.describe MsTeamsHermes::Components::TextBlock do
     let(:font_type) { MsTeamsHermes::Style::FontType::MONOSPACE }
     let(:weight) { MsTeamsHermes::Style::FontWeight::BOLDER }
     let(:wrap) { true }
+    let(:is_subtle) { true }
 
     it "renders the hash object" do
       hash = {
@@ -37,7 +39,8 @@ RSpec.describe MsTeamsHermes::Components::TextBlock do
         size: size,
         font_type: font_type,
         weight: weight,
-        wrap: wrap
+        wrap: wrap,
+        is_subtle: is_subtle
       }
 
       expect(component.to_hash).to eq hash
