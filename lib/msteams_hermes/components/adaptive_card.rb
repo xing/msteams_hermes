eaa8b10b549d012705c60e070e748f4e87937a02
Michael Jäger <2405937+huNt-FMJ@users.noreply.github.com>
2023-01-17 17:48:57 +0100

    [adaptive_card.rb]: Add support for entities which encapsulate the mention type https://learn.microsoft.com/en-us/microsoftteams/platform/task-modules-and-cards/cards/cards-format?tabs=adaptive-md%2Cdesktop%2Cconnector-html#mention-support-within-adaptive-cards

--- lib/msteams_hermes/components/adaptive_card.rb
+++ lib/msteams_hermes/components/adaptive_card.rb
@@ -12,14 +12,17 @@ module MsTeamsHermes
     # https://adaptivecards.io/explorer/AdaptiveCard.html
     ##
     class AdaptiveCard < Base
-      attr_reader :body, :actions
+      attr_reader :body, :actions, :entities
 
-      def initialize(body:, actions: nil)
+      def initialize(body:, actions: nil, entities: nil)
         @body = body
         raise "AdaptiveCard `body` must be an Array" unless @body.is_a? Array
 
         @actions = actions
         raise "AdaptiveCard `actions` must be an Array" unless @actions.nil? || @actions.is_a?(Array)
+
+        @entities = entities
+        raise "AdaptiveCard `entities` must be an Array" unless @entities.nil? || @entities.is_a?(Array)
       end
 
       def to_hash
@@ -27,7 +30,8 @@ module MsTeamsHermes
           type: "AdaptiveCard",
           version: "1.5",
           body: body.map(&:to_hash),
-          actions: actions&.map(&:to_hash)
+          actions: actions&.map(&:to_hash),
+          msteams: {entities: entities&.map(&:to_hash)}
         }
       end
     end
