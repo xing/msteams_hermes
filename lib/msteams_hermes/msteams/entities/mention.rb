a40232ba337fb79fc54b4d2a3f532e3eaf25ccde
Nej <2405937+huNt-FMJ@users.noreply.github.com>
2023-01-18 16:39:00 +0100

    Update lib/msteams_hermes/msteams/entities/mention.rb
    Co-authored-by: Rafael Rocha <103581636+rafaelatxing@users.noreply.github.com>

--- lib/msteams_hermes/msteams/entities/mention.rb
+++ lib/msteams_hermes/msteams/entities/mention.rb
@@ -10,6 +10,8 @@ module MsTeamsHermes
       # https://learn.microsoft.com/en-us/microsoftteams/platform/task-modules-and-cards/cards/cards-format?tabs=adaptive-md%2Cdesktop%2Cconnector-html#user-mention-in-incoming-webhook-with-adaptive-cards
       ##
       class Mention < Base
+        attr_reader :mention_reference, :mention_string, :user_id
+
         def initialize(text:, name:, id:)
           raise "`text` must be a string" unless text.is_a? String
           raise "`text` must contain <at>...</at>" unless text.include?("<at>") && text.include?("</at>")
