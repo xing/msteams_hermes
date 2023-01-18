f20280863ecdfcfd1e28ca1cab515f811028006e
Michael Jäger <2405937+huNt-FMJ@users.noreply.github.com>
2023-01-18 16:55:38 +0100

    [mention_spec.rb]: Add rspec test of 'to_hash'

--- spec/msteams_hermes/entities/mention_spec.rb
+++ spec/msteams_hermes/entities/mention_spec.rb
@@ -4,10 +4,33 @@ require "msteams_hermes/msteams/entities/mention"
 
 RSpec.describe MsTeamsHermes::MsTeams::Entities::Mention do
   describe "#initialize" do
-    it "throws an error if text paramter does not contain <at> and </at>" do
+    it "throws an error if text parameter does not contain <at> and </at>" do
       expect do
         MsTeamsHermes::MsTeams::Entities::Mention.new(text: "foo", name: "something", id: "123")
       end.to raise_error("`text` must contain <at>...</at>")
+    end
+  end
+
+  describe "#to_hash" do
+    subject(:mention) { MsTeamsHermes::MsTeams::Entities::Mention.new(text: mention_segment_in_text,
+                                                                      name: mention_string,
+                                                                      id: user_id) }
+
+    let(:mention_segment_in_text) { "<at>original text reference</at>" }
+    let(:user_id) { "aneamailaddress@yourcompany.com" }
+    let(:mention_string) { "Text to show up as mention" }
+
+    it "renders the hash object" do
+      expected_hash = {
+        type: "mention",
+        text: mention_segment_in_text,
+        mentioned: {
+          id: user_id,
+          name: mention_string
+        }
+      }
+
+      expect(mention.to_hash).to eq(expected_hash)
     end
   end
 end
